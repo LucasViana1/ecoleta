07fe660a29460cfd81b9c9cec7cb0e32c56ec5f5
Lucas Viana <lucasviana112@gmail.com>
2020-06-03 18:22:51 -0300

    aula 2 - estruturando backend, definindo rotas

--- server/README.md
+++ server/README.md
@@ -12,7 +12,19 @@ run server static
 npx ts-node .\src\server.ts
 
 run server listen dev
-npx ts-node-dev .\src\server.ts
+npx ts-node-dev --transpileOnly --ignore-watch node_modules src/server.ts
 
 run server script
 npm run dev
+
+run migration
+npx knex --knexfile knexfile.ts migrate:latest
+
+run migration script
+npm run knex:migration
+
+run seed
+knex --knexfile knexfile.ts seed:run
+
+run seed script
+npm run knex:seed
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,38 @@
 import express from "express";
+import cors from "cors";
+import path from "path";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  response.json({ text: "ola!!" });
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
+
+// // rotas exemplos
+// const users = ["Lucas", "Viana", "Cunha"];
+
+// app.get("/users", (request, response) => {
+//   const search = String(request.query.search);
+
+//   const filteredUsers = search
+//     ? users.filter((user) => user.includes(search))
+//     : users;
+
+//   return response.json(filteredUsers);
+// });
+
+// app.get("/users/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   return response.json(users[id]);
+// });
+
+// app.post("/users", (request, response) => {
+//   const data = request.body;
+
+//   return response.json(data);
+// });
